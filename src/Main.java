0919a7e1c728f0486c709b3022e08d18e88ff1c5
LOKI <sanyaproskill@gmail.com>
2023-06-02 16:22:37 +0300

    implementation of the sequence of cities by the final letter

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String fileName = "D:\\java\\work\\cities.txt";
+        List<String> cities = readCitiesFromFile(fileName);
+
+        if (!cities.isEmpty()) {
+            List<String> longestChain = findLongestWordChain(cities);
+            printWordChain(longestChain);
+        } else {
+            System.out.println("Файл порожній або не вдалося прочитати.");
+        }
+    }
+
+    private static List<String> readCitiesFromFile(String fileName) {
+        List<String> cities = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                cities.add(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return cities;
+    }
+
+    private static List<String> findLongestWordChain(List<String> cities) {
+        return cities.stream()
+                .map(city -> {
+                    List<String> chain = new ArrayList<>();
+                    chain.add(city);
+                    findNextWord(city, cities, chain);
+                    return chain;
+                })
+                .max(Comparator.comparingInt(List::size))
+                .orElse(Collections.emptyList());
+    }
+
+    private static void findNextWord(String currentWord, List<String> cities, List<String> chain) {
+        char lastChar = Character.toLowerCase(currentWord.charAt(currentWord.length() - 1));
+
+        Optional<String> nextWord = cities.stream()
+                .filter(city -> !chain.contains(city))
+                .filter(city -> Character.toLowerCase(city.charAt(0)) == lastChar)
+                .findFirst();
+
+        nextWord.ifPresent(word -> {
+            chain.add(word);
+            findNextWord(word, cities, chain);
+        });
+    }
+
+    private static void printWordChain(List<String> chain) {
+        System.out.println(String.join(" ", chain));
+    }
+}
